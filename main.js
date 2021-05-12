281d437b1078ee5bdd1408eeacbc5cdd64a83208
manishkheta321 <manish.khetan123456789@gmail.com>
2021-05-12 23:54:42 +0530

    updated cf profile

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ about.addEventListener("click", () => {
   const aboutBox = new WinBox({
     title: "About Me",
     background: "#00aa00",
-    width: "400px",
+    width: "600px",
     height: "400px",
     top: 50,
     right: 50,
@@ -29,7 +29,7 @@ contact.addEventListener("click", () => {
   const contactBox = new WinBox({
     title: "Contact Me",
     background: "#00aa00",
-    width: "400px",
+    width: "600px",
     height: "400px",
     top: 50,
     right: 50,
@@ -46,6 +46,25 @@ contact.addEventListener("click", () => {
     },
   });
 });
+const codeforcesContent = document.querySelector(".codeforces");
 function codeforces() {
-  new WinBox();
+  const codeforcesBox = new WinBox({
+    title: "CF Profile",
+    background: "#00aa00",
+    width: "600px",
+    height: "500px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    x: 250,
+    y: 150,
+    mount: codeforcesContent,
+    onfocus: function () {
+      this.setBackground("#00aa00");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
 }
